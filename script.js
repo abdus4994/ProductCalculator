2099f4cef5e72efb08fc84233c50fb7f540e9da2
Md. Abdus Salam <132735606+abdus4994@users.noreply.github.com>
2024-02-19 02:43:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,62 @@ function reset(){
     
    
 };
+
+
+function display1(){
+   $("#btn-position").html(`
+   <div id="btn-totalQty" class="row text-center">
+   <div>
+       <button class="btn m-1 btn-primary" onclick="digit(7)">7</button>
+       <button class="btn m-1 btn-primary" onclick="digit(8)">8</button>
+       <button class="btn m-1 btn-primary" onclick="digit(9)">9</button>
+   </div>
+   <div>
+       <button class="btn m-1 btn-primary" onclick="digit(4)" >4</button>
+       <button class="btn m-1 btn-primary" onclick="digit(5)">5</button>
+       <button class="btn m-1 btn-primary" onclick="digit(6)">6</button>
+   </div>
+   <div>
+       <button class="btn m-1 btn-primary" onclick="digit(1)">1</button>
+       <button class="btn m-1 btn-primary" onclick="digit(2)">2</button>
+       <button class="btn m-1 btn-primary" onclick="digit(3)">3</button>
+   </div>
+ </div>
+
+   `)
+}
+
+function display2(){
+    $("#btn-position").html(`
+    <div id="btn-totalQty" class="row text-center">
+    <div>
+        <button class="btn m-1 btn-primary" onclick="insertNumber(7)">7</button>
+        <button class="btn m-1 btn-primary" onclick="insertNumber(8)">8</button>
+        <button class="btn m-1 btn-primary" onclick="insertNumber(9)">9</button>
+    </div>
+    <div>
+        <button class="btn m-1 btn-primary" onclick="insertNumber(4)" >4</button>
+        <button class="btn m-1 btn-primary" onclick="insertNumber(5)">5</button>
+        <button class="btn m-1 btn-primary" onclick="insertNumber(6)">6</button>
+    </div>
+    <div>
+        <button class="btn m-1 btn-primary" onclick="insertNumber(1)">1</button>
+        <button class="btn m-1 btn-primary" onclick="insertNumber(2)">2</button>
+        <button class="btn m-1 btn-primary" onclick="insertNumber(3)">3</button>
+    </div>
+  </div>
+ 
+    `)
+ }
+
+    function digit(number){
+        let existingNumber = $("#totalQty").val();
+        $("#totalQty").val(existingNumber + number);     
+    }
+
+    function insertNumber(value){
+        let existingNumber = $("#ctnPerPcs").val();
+        $("#ctnPerPcs").val(existingNumber + value); 
+       console.log(value)  
+    }
+ 
