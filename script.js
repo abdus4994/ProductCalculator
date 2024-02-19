ffd7ad6c712f6d819f21a800729af53fdcb6f472
Md. Abdus Salam <132735606+abdus4994@users.noreply.github.com>
2024-02-19 10:14:57 +0600

    Update script.js

--- script.js
+++ script.js
@@ -24,20 +24,25 @@ function display1(){
    $("#btn-position").html(`
    <div id="btn-totalQty" class="row text-center">
    <div>
-       <button class="btn m-1 btn-primary" onclick="digit(7)">7</button>
-       <button class="btn m-1 btn-primary" onclick="digit(8)">8</button>
-       <button class="btn m-1 btn-primary" onclick="digit(9)">9</button>
+       <button class="btn m-2 px-3 fs-4 btn-primary" onclick="digit(7)">7</button>
+       <button class="btn m-2 px-3 fs-4 btn-primary" onclick="digit(8)">8</button>
+       <button class="btn m-2 px-3 fs-4 btn-primary" onclick="digit(9)">9</button>
    </div>
    <div>
-       <button class="btn m-1 btn-primary" onclick="digit(4)" >4</button>
-       <button class="btn m-1 btn-primary" onclick="digit(5)">5</button>
-       <button class="btn m-1 btn-primary" onclick="digit(6)">6</button>
+       <button class="btn m-2 px-3 fs-4 btn-primary" onclick="digit(4)" >4</button>
+       <button class="btn m-2 px-3 fs-4 btn-primary" onclick="digit(5)">5</button>
+       <button class="btn m-2 px-3 fs-4 btn-primary" onclick="digit(6)">6</button>
    </div>
    <div>
-       <button class="btn m-1 btn-primary" onclick="digit(1)">1</button>
-       <button class="btn m-1 btn-primary" onclick="digit(2)">2</button>
-       <button class="btn m-1 btn-primary" onclick="digit(3)">3</button>
+       <button class="btn m-2 px-3 fs-4 btn-primary" onclick="digit(1)">1</button>
+       <button class="btn m-2 px-3 fs-4 btn-primary" onclick="digit(2)">2</button>
+       <button class="btn m-2 px-3 fs-4 btn-primary" onclick="digit(3)">3</button>
    </div>
+   <div>
+   
+   <button class="btn m-2 px-3 fs-4 btn-primary" onclick="digit(0)">0</button>
+   
+</div>
  </div>
 
    `)
@@ -47,19 +52,23 @@ function display2(){
     $("#btn-position").html(`
     <div id="btn-totalQty" class="row text-center">
     <div>
-        <button class="btn m-1 btn-primary" onclick="insertNumber(7)">7</button>
-        <button class="btn m-1 btn-primary" onclick="insertNumber(8)">8</button>
-        <button class="btn m-1 btn-primary" onclick="insertNumber(9)">9</button>
+        <button class="btn m-2 px-3 fs-4 btn-primary" onclick="insertNumber(7)">7</button>
+        <button class="btn m-2 px-3 fs-4 btn-primary" onclick="insertNumber(8)">8</button>
+        <button class="btn m-2 px-3 fs-4 btn-primary" onclick="insertNumber(9)">9</button>
     </div>
     <div>
-        <button class="btn m-1 btn-primary" onclick="insertNumber(4)" >4</button>
-        <button class="btn m-1 btn-primary" onclick="insertNumber(5)">5</button>
-        <button class="btn m-1 btn-primary" onclick="insertNumber(6)">6</button>
+        <button class="btn m-2 px-3 fs-4 btn-primary" onclick="insertNumber(4)" >4</button>
+        <button class="btn m-2 px-3 fs-4 btn-primary" onclick="insertNumber(5)">5</button>
+        <button class="btn m-2 px-3 fs-4 btn-primary" onclick="insertNumber(6)">6</button>
     </div>
     <div>
-        <button class="btn m-1 btn-primary" onclick="insertNumber(1)">1</button>
-        <button class="btn m-1 btn-primary" onclick="insertNumber(2)">2</button>
-        <button class="btn m-1 btn-primary" onclick="insertNumber(3)">3</button>
+        <button class="btn m-2 px-3 fs-4 btn-primary" onclick="insertNumber(1)">1</button>
+        <button class="btn m-2 px-3 fs-4 btn-primary" onclick="insertNumber(2)">2</button>
+        <button class="btn m-2 px-3 fs-4 btn-primary" onclick="insertNumber(3)">3</button>
+    </div>
+    <div>
+    
+        <button class="btn m-2 px-3 fs-4 btn-primary" onclick="insertNumber(0)">0</button>    
     </div>
   </div>
  
@@ -74,6 +83,6 @@ function display2(){
     function insertNumber(value){
         let existingNumber = $("#ctnPerPcs").val();
         $("#ctnPerPcs").val(existingNumber + value); 
-       console.log(value)  
+        
     }
  
